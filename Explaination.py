482e32b89966537dd31aba7d2f1c5104c3f78c96
GENG Minghong <58876849+GENGMinghong@users.noreply.github.com>
2020-07-26 18:50:27 +0800

    Update Explaination.py

--- Explaination.py
+++ Explaination.py
@@ -8,11 +8,6 @@ Created on Wed Jul 22 17:29:56 2020
 
 # 1. Import Packages
 import difflib
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk import WordNetLemmatizer
-from nltk.stem import PorterStemmer 
-from nltk.corpus import wordnet
 import pandas as pd
 import errant
 
@@ -21,20 +16,6 @@ import errant
 
 # 2. Define Functions 
 # 2.1 Get the Action: insert, remove, and replace
-
-# =============================================================================
-# This part was weitten by Yijun. Now it is deprecated.
-# def get_action(s1,s2):
-#     m = len(s1.split())
-#     n = len(s2.split())
-#     if m>n:
-#         return 'remove'
-#     elif m<n:
-#         return 'insert'
-#     else:
-#         return 'replace'
-# =============================================================================
-
 def get_action(s1,s2):
     annotator = errant.load('en')
     orig = annotator.parse(s1)
@@ -48,60 +29,17 @@ def get_action(s1,s2):
         elif 'U:' in e.type:
             return 'remove'
         
-# 2.2 Get the change made py GEC    
-            
-# =============================================================================
-# # This part was weitten by Yijun. Now it is deprecated.
-# # Aim to get 3 words, the word before the error in sentence 1, the word in the error position of correct sentence and the wrong sentence   
-# def get_difference(s1,s2):
-#     temp1 = s1.split()
-#     temp2 = s2.split()
-#     for i in range(min(len(temp1),len(temp2))):
-#         if temp1[i] != temp2[i]:
-#             return temp1[i-1],temp1[i],temp2[i] # return a tuple
-# =============================================================================
-            
+# 2.2 Get the change made py GEC                
 def get_diff(a,b):
-    if len(a)>len(b): 
-        res=''.join(a.split(b))             
-    else: 
-        res=''.join(b.split(a))             
-    return res.strip()
+    s= [li for li in difflib.ndiff(a, b) if li[0] != ' ']
+    return s
 
 # 2.3 Get the category of the correction, there are 
 # This part is used to get the category of the correction
-# function to convert nltk tag to wordnet tag
+# Get the Category of errors, there are 6 types all together.
+punc=['- .','+ .','- ,','+ ,',"- '","+ '", '- ;','+ ;','- -','+ -','-  ','+  ']
+ing = ['+ i', '+ n', '+ g']
 
-# =============================================================================
-# def nltk_tag_to_wordnet_tag(nltk_tag):
-#     if nltk_tag.startswith('J'):
-#         return wordnet.ADJ
-#     elif nltk_tag.startswith('V'):
-#         return wordnet.VERB
-#     elif nltk_tag.startswith('N'):
-#         return wordnet.NOUN
-#     elif nltk_tag.startswith('R'):
-#         return wordnet.ADV
-#     else:          
-#         return None
-# 
-# def lemmatize_sentence(sentence):
-#     #tokenize the sentence and find the POS tag for each token
-#     nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))  
-#     #tuple of (token, wordnet_tag)
-#     wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
-#     lemmatized_sentence = []
-#     for word, tag in wordnet_tagged:
-#         if tag is None:
-#             #if there is no available tag, append the token as is
-#             lemmatized_sentence.append(word)
-#         else:        
-#             #else use the tag to lemmatize the token
-#             lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
-#     return " ".join(lemmatized_sentence)
-# =============================================================================
-
-punctuation = ['.',',','!','?','..','...','-']
 def get_category(s1,s2):
     annotator = errant.load('en')
     orig = annotator.parse(s1)
@@ -112,66 +50,21 @@ def get_category(s1,s2):
             return 'Articles'
         elif 'PREP' in e.type or 'PART' in e.type:
             return 'Preposition'
-        elif 'PUNCT' in e.type or (e.o_str!='' and e.c_str!='' and get_diff(e.o_str,e.c_str) in punctuation) or ('-' in e.o_str or '-' in e.c_str):
+        elif 'PUNCT' in e.type or get_diff(e.o_str, e.c_str)[0] in punc:
             return 'Punctuation'
-        elif 'VERB' in e.type:
+        elif 'VERB' in e.type or set(ing).issubset(set(get_diff(e.o_str, e.c_str))):
             if 'SVA' in e.type:
                 return 'Subject Verb Agreement'
             else:
                 return 'Verb Form'
-        elif 'NOUN' in e.type or 'ADJ' in e.type or 'MORPH' in e.type:
+        elif 'NOUN' in e.type or 'ADJ' in e.type or 'MORPH' in e.type or 'SPELL' in e.type or 'ORTH' in e.type:
             return 'Word Form'
         else:
             return 'Other'
 
-# Get the Category of errors, there are 6 types.
-# =============================================================================
-# def get_category(s1,s2):
-#     pre = get_difference(s1,s2)[0] # get the word right before the word we change
-#     diff = get_difference(s1,s2)[2] # get the word revised
-#     original = get_difference(s1,s2)[1] # get the original word before being revised
-#     
-#     diff_ratio = difflib.SequenceMatcher(None, original, diff).ratio() 
-#     
-#     #Calculate the same ratio of the original word and revised word
-#     p = list(set(word_tokenize(diff))-set(word_tokenize(original)))
-#     if p==[]:
-#         p = list(set(word_tokenize(original))-set(word_tokenize(diff)))    
-#   
-#     action = get_action(s1,s2)
-#     
-#     if action == 'replace':
-#         s = pre+' '+diff # 2 words from the revised sentence
-#         #tag = nltk.pos_tag(word_tokenize(s))
-#         tag = nltk.pos_tag(word_tokenize(lemmatize_sentence(s)))
-#         if tag[0][1] in nountag and tag[1][1] in verbtag: # this method depends on the correct pos tagging, but the accuracy is not high enough.
-#                return 'Subject Verb Agreement'
-#         elif p[0] in punctuation:
-#                return 'Punctuation'
-#     else:
-#         if diff in article:
-#             return 'article'
-#         elif diff in preposition:
-#             return 'preposition'
-#         elif diff in subject_verb_agreement:
-#             return 'subject verb agreement'
-#         elif diff_ratio>=0.5:
-#             return 'Word Form'
-# =============================================================================
-        
-# define 6 type of words
-# =============================================================================
-# article = ['a','an','the']
-# preposition = ['by','to','in']
-# subject_verb_agreement = ['has','have','is','are','am', 'generates']
-# nountag = ['PRP','NN','NNS','NNP','NNPS']
-# verbtag = ['VBP','VBD','VBG','VBN','VBP','VBZ']
-# punctuation = ['.',',','!','?','..','...']
-# =============================================================================
 
-
-# Get The Explaination of Correction
-# This function fill make use of the get_action and get_difference function.
+# Get The Explaination of Correction made by our Grammar Correction Engine
+# This function will make use of the get_action() and get_difference() function.
 def get_explanation(s1,s2):
     annotator = errant.load('en')
     orig = annotator.parse(s1)
@@ -181,21 +74,70 @@ def get_explanation(s1,s2):
     if error == 'Verb Form':
         for e in edits:
             if 'TENSE' in e.type:
-                return 'Verb Tense Error.'
+                if 'R:' in e.type:
+                    return f"Verb tense error, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Verb tense error, should insert '{e.c_str}'."
+                else:
+                    return f"Verb tense error, should remove '{e.o_str}'."
             elif 'FORM' in e.type:
-                return 'Verb Form error.'
+                if 'R:' in e.type:
+                    return f"Verb form error, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Verb form error, should insert '{e.c_str}'."
+                else:
+                    return f"Verb form error, should remove '{e.o_str}'."
+            elif set(ing).issubset(set(get_diff(e.o_str, e.c_str))):
+                if 'R:' in e.type:
+                    return f"Present continuous tense, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Present continuous tense, should insert '{e.c_str}'."
+                else:
+                    return f"Present continuous tense, should remove '{e.o_str}'."
             else:
-                return 'Other verb error.'
+                if 'R:' in e.type:
+                    return f"Other verb error, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Other verb error, should insert '{e.c_str}'."
+                else:
+                    return f"Other verb error, should remove '{e.o_str}'."
     elif error == 'Word Form':
         for e in edits:
             if 'NUM' in e.type:
-                return 'Noun Number error.'
+                if 'R:' in e.type:
+                    return f"Noun Number error, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Noun Number error, should insert '{e.c_str}'."
+                else:
+                    return f"Noun Number error, should remove '{e.o_str}'."
             elif 'ADJ' in e.type:
-                return 'Adjective error.'
+                if 'R:' in e.type:
+                    return f"Adjective error, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Adjective error, should insert '{e.c_str}'."
+                else:
+                    return f"Adjective error, should remove '{e.o_str}'."
             elif 'MORPH' in e.type:
-                return 'Morphology error.'
+                if 'R:' in e.type:
+                    return f"Morphology error, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Morphology error, should insert '{e.c_str}'."
+                else:
+                    return f"Morphology error, should remove '{e.o_str}'."
+            elif 'ORTH' in e.type:
+                if 'R:' in e.type:
+                    return f"Orthography error, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Orthography error, should insert '{e.c_str}'."
+                else:
+                    return f"Orthography error, should remove '{e.o_str}'."
             else:
-                return 'Other word form error.'
+                if 'R:' in e.type:
+                    return f"Other word form error, should replace '{e.o_str}' with '{e.c_str}'."
+                elif 'M:' in e.type:
+                    return f"Other word form error, should insert '{e.c_str}'."
+                else:
+                    return f"Other word form error, should remove '{e.o_str}'."
     elif error == 'Punctuation':
         for e in edits:
             if 'M:' in e.type:
@@ -237,7 +179,7 @@ def get_explanation(s1,s2):
                 return 'Others'
 
 # 3. Import Data
-# The Data Imported here is the 
+# The Data Imported here is the test data with correct sentences generated by GEC system.
 data = pd.read_csv('data/PowerGrammarQuestionBank_correction.csv')
 
 data['give_action'] = data.apply(lambda x: get_action(x.question, x.correction_by_GEC),axis=1)
@@ -269,4 +211,3 @@ get_explanation(s7,s8)
 s9 = data['question'].iloc[8]
 s10 = data['solution'].iloc[8]
 get_explanation(s9,s10)
-
